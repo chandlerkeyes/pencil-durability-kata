c524cae2dbcea328329a3112a8ff5e2cc0c8824d
chandlerkeyes <chandler.keyes@interactiongaming.com>
2019-08-08 06:41:13 -0400

    First test: minimally required code to make first test pass

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export default class Pencil {
+  writeOnPaper(paper, textToWrite) {
+    return "Hello World!";
+  }
+};
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,11 @@
 import { expect } from "chai"
+import Pencil from "../src/";
 
 describe("Pencil Durability Kata Tests", () => {
+  it("Writes Hello World! on a blank piece of paper", () => {
+    const pencil = new Pencil();
+    const result = pencil.writeOnPaper("", "Hello World!");
+
+    expect(result).to.equal("Hello World!");
+  });
 });
